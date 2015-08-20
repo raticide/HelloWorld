4cdb083437b93b32e9163d7952334481c0e03bf2
Benoit Durand <benoit.durand1@fr.ibm.com>
2015-08-20 14:58:30 +0200

    Add class Hello

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Benoit Durand
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world !");
+	}
+
+}
